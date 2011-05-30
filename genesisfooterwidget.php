fcb46812743f1dd675d3a416ff87b85b2737e0c4
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-30 17:24:22 +0000

    initial version
    git-svn-id: https://plugins.svn.wordpress.org/genesis-footer-widgets/trunk@390873 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ genesisfooterwidget.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Genesis Footer Widgets
+Plugin URI: http://www.ramoonus.nl
+Description: This plugin adds 3 footer widgets to Genesis 1.6 and higher. 
+Version: 1.0.0
+Author: Ramoonus
+Author URI: http://www.ramoonus.nl/wordpress/genesis-footer-widgets/
+*/
+function genesisfooterwidgetsplugin() {
+	/** Add support for 3-column footer widgets **/
+	add_theme_support('genesis-footer-widgets', 3 );
+	
+	// great job, you`ve got support for 3 unstyled widgets, better do some styling
+	/* CSS */ 
+	wp_deregister_style('genesis-footer-widgets-style'); // deregister
+	wp_register_style('genesis-footer-widgets-style', plugins_url('/css/gfw.css', __FILE__), false, '1.0.0'); // re register
+	wp_enqueue_style('genesis-footer-widgets-style'); // load
+			
+	// powered by http://dev.studiopress.com/add-widgeted-footer.htm 
+}
+add_action('init', 'genesisfooterwidgetsplugin');
+?>
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Plugin Name ===
+Contributors: ramoonus
+Donate link: http://www.ramoonus.nl
+Tags: genesis, genesiswp, footer, widgets, widget
+Requires at least: 3.0.0
+Tested up to: 3.2
+Stable tag: 1.0.0
+
+This plugin adds 3 footer widgets to Genesis 1.6 and higher. 
+== Description ==
+This plugin adds 3 footer widgets to Genesis 1.6 and higher. 
+For this plugin to work you will need to have the Genesis theme installed and use it - or a child theme of Genesis.
+
+== Installation ==
+1. Upload `gfw/` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions == 
+None at this moment.
+
+== Upgrade Notice == 
+Its recommended to flush the cache after upgrading.
+
+== Screenshots ==
+Not relevant.
+
+== Changelog ==
+= 1.0 =
+* First version
