9c5b71afd9602c76c0e109d9fe0c64e72c1cd23a
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-27 09:40:57 +0000

    version 1.1 in trunk
    git-svn-id: https://plugins.svn.wordpress.org/genesis-footer-widgets/trunk@511036 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- genesisfooterwidget.php
+++ genesisfooterwidget.php
@@ -2,24 +2,151 @@
 /*
 Plugin Name: Genesis Footer Widgets
 Plugin URI: http://www.ramoonus.nl/wordpress/genesis-footer-widgets/
-Description: This plugin adds 3 footer widgets to Genesis 1.6 and higher. 
-Version: 1.0.1
+Description: Adds 3 or 4 footer widget areas to the Genesis Theme Framework version 1.6 or higher. 
+Version: 1.1
 Author: Ramoonus
 Author URI: http://www.ramoonus.nl/
 */
-function genesisfooterwidgetsplugin() {
-	/** Add support for 3-column footer widgets **/
 
+// Declare variables
+$gfw_version = '1.1';
+
+// Initialize function
+function gfw_init() {
+	// CSS
+	wp_deregister_style('gfw-core');
+	wp_enqueue_style('gfw-core', plugins_url('/css/gfw.css', __FILE__), false, $gfw_version);
 	
-	// great job, you`ve got support for 3 unstyled widgets, better do some styling
-	/* CSS */ 
-	wp_deregister_style('genesis-footer-widgets-style'); // deregister
-	wp_register_style('genesis-footer-widgets-style', plugins_url('/css/gfw.css', __FILE__), false, '1.0.0'); // re register
-	wp_enqueue_style('genesis-footer-widgets-style'); // load
-			
-	// powered by http://dev.studiopress.com/add-widgeted-footer.htm 
+	// Textdomain
+	load_plugin_textdomain( 'gfw', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );	
+
+	// Load Options	
+	$gfw_option = get_option('gfw');
+	
+	// CSS part 2
+	if ($gfw_option = 3) { 
+		wp_deregister_style('gfw3');
+		wp_enqueue_style('gfw3', plugins_url('/css/gfw3.css', __FILE__), false, $gfw_version);
+	}
+	elseif ($gfw_option = 4) {  
+		wp_deregister_style('gfw4');
+		wp_enqueue_style('gfw4', plugins_url('/css/gfw4.css', __FILE__), false, $gfw_version);
+	}
+	
 }
-add_action('init', 'genesisfooterwidgetsplugin');
-// init the widgets themself
-add_theme_support('genesis-footer-widgets', 3 );
+add_action('init', 'gfw_init');
+	
+	
+function gfw_admin_style(){
+        wp_enqueue_style( 'gfw-admin', plugins_url('/gfw-admin.js', __FILE__) , false, $gfw_version );
+}
+add_action('admin_enqueue_scripts', 'gfw_admin_style');
+
+
+// Activation
+function gfw_activate() {
+	// Detect if genesis is loaded
+	gwf_detect_genesis();
+	// Install option	
+	add_option('gfw', '3', '', 'yes');
+	// Flush Cache
+	gfw_flushcache(); 
+}
+register_activation_hook( __FILE__, 'gfw_activate' );
+
+// Deactivation
+function gfw_deactivate() {
+	// Delete option	
+	delete_option('gfw');
+	// Flush Cache
+	gfw_flushcache(); 
+}
+register_deactivation_hook( __FILE__, 'gfw_deactivate' );
+
+// Flush Cache function
+function gfw_flushcache() {
+	// W3TC
+	// WP SC
+}
+
+function gfw_core() {
+	// Retrieve Options
+	$gfw_option = get_option('gfw');
+	
+	// if empty, activation didn`t work and disable
+	if (!$gfw_option) { 
+		wp_die('Genesis Footer Widgets Error: Unable to retrieve options');
+		deactivate_plugins( plugin_basename( __FILE__ ) );  // Deactivate ourself
+	}
+	
+	// Detect if option is anything else than 3 or 4
+	if( !$gfw_option = 3 || !$gfw_option = 4 ) {
+		wp_die('Genesis Footer Widgets Error: Options are corrupt');
+		deactivate_plugins( plugin_basename( __FILE__ ) );  // Deactivate ourself
+	}
+	
+	if ($gfw_option = 3) { 
+		/** Add support for 3-column footer widgets **/
+		add_theme_support('genesis-footer-widgets', 3 );
+	}
+	if ($gfw_option = 4) {  
+		/** Add support for 4-column footer widgets **/
+		add_theme_support('genesis-footer-widgets', 4 );
+	}
+	
+}
+add_action('after_setup_theme', 'gfw_core');
+
+
+/* Detect if template is genesis */
+function gwf_detect_genesis() {	
+	// versions
+	$latest = '1.7.1';
+	$minimum = '1.6';
+	
+	if ( basename( get_template_directory() ) != 'genesis' ) {		
+		deactivate_plugins( plugin_basename( __FILE__ ) );  // Deactivate ourself
+		wp_die( 
+			sprintf( __( 'You need to have installed and enabled the %1$sGenesis Framework%2$s', 'gfw'), '<a href="http://www.studiopress.com/themes/genesis" target="_new">', '</a>' ) );	
+	} // end of if
+} // end of function
+
+// Menu Item
+function gfw_menu() {
+		add_submenu_page('genesis', 'Genesis - Footer Widgets', 'Footer Widgets', 'manage_options', 'gfw-options', 'gfw_admin_page' );
+}
+add_action('admin_menu', 'gfw_menu');
+
+// Menu Page
+function gfw_admin_page() {
+	$gfw_option = get_option('gfw');
+	
+	{  ?>
+
+<div class="wrap" class="gfw-admin">
+  <h2>Genesis Footer Widgets</h2>
+  <form method="post" action="options.php">
+    <?php wp_nonce_field('update-options') ?>
+    <p><strong>Amount of widgets (either 3 or 4):</strong><br />
+      <input type="text" name="gfw" size="1" value="<?php echo get_option('gfw'); ?>" />
+    </p>
+    <p>
+      <input type="submit" name="Submit" value="Store Options" />
+    </p>
+    <input type="hidden" name="action" value="update" />
+    <input type="hidden" name="page_options" value="gfw" />
+  </form>
+  
+<h3>Help, I don`t see the widgets</h3>
+<p>Visit the <a href="widgets.php">widgets</a> page.</p>
+</div>
+<?php  }	// end of echo
+}			// end of function
+
+
+
+
+
+
+
 ?>
--- readme.txt
+++ readme.txt
@@ -1,32 +1,50 @@
-=== Plugin Name ===
-Contributors: ramoonus
-Donate link: http://www.ramoonus.nl
+=== Genesis Footer Widgets ===
+Contributors: Ramoonus
+Donate link: http://www.ramoonus.nl/donate/
 Tags: genesis, genesiswp, footer, widgets, widget
 Requires at least: 3.0.0
-Tested up to: 3.2
-Stable tag: 1.0.1
+Tested up to: 3.3.1
+Stable tag: 1.1
 
-This plugin adds 3 footer widgets to Genesis 1.6 and higher. 
+Adds 3 or 4 footer widget areas to the Genesis Theme Framework version 1.6 or higher. 
 == Description ==
-This plugin adds 3 footer widgets to Genesis 1.6 and higher. 
+Adds 3 or 4 footer widget areas to the Genesis Theme Framework version 1.6 or higher.  
+
 For this plugin to work you will need to have the Genesis theme installed and use it - or a child theme of Genesis.
+This plugin will auto-disable if these requirements are not met.
 
 == Installation ==
-1. Upload `gfw/` to the `/wp-content/plugins/` directory
+1. Upload `genesis-footer-widgets/` to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add widgets
 
 == Frequently Asked Questions == 
 None at this moment.
 
 == Upgrade Notice == 
-Its recommended to flush the cache after upgrading.
+= 1.1 =
+Major new release. Adds the option to switch between 3 and 4 footer widgets and more!
+
+= 1.0.1 =
+* Fixes  reported bugs.
 
 == Screenshots ==
-Not relevant.
+1. Widget Menu containing 4 widgets
+
+1. Widget Menu containing 3 widgets
 
 == Changelog ==
+= 1.1 =
+Major new release, includes the following new features and changes:
+
+* Options screen, allows the user to switch between 3 and 4 footer widgets
+* Requirement checker. Verifies if Genesis (theme) is installed
+
+= 1.0.2 =
+* Detects if Genesis is present and active
+
 = 1.0.1 =
-* Fixes the reported bugs.
+* Fixes reported bugs.
 
 = 1.0 =
 * First version
